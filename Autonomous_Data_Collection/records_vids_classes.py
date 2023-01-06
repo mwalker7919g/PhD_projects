58fcfcba00b70b5cb1f276a155f60891d2fa73f1
mwalker7919g <122103847+mwalker7919g@users.noreply.github.com>
2023-01-06 17:52:45 +0000

    Update records_vids_classes.py

--- Autonomous_Data_Collection/records_vids_classes.py
+++ Autonomous_Data_Collection/records_vids_classes.py
@@ -8,10 +8,10 @@ class RecordVideo():
     def __init__(self, video_filename, capture_duration):
 
         self.open = True
-        self.capture_duration = capture_duration
-        self.device_index, self.fps, self.fourcc, self.frameSize = 0, 27, "MJPG", (640, 480)
+        self.capture_duration = capture_duration # length of video
+        self.device_index, self.fps, self.fourcc, self.frameSize = 0, 27, "MJPG", (640, 480) # video parameters
         self.video_filename = video_filename
-        self.video_cap, self.video_writer = cv2.VideoCapture(self.device_index), cv2.VideoWriter_fourcc(*self.fourcc)
+        self.video_cap, self.video_writer = cv2.VideoCapture(self.device_index), cv2.VideoWriter_fourcc(*self.fourcc) # save video
         self.video_out = cv2.VideoWriter(self.video_filename, self.video_writer, self.fps, self.frameSize)
         self.frame_counts = 1
         self.start_time = time.time()
@@ -44,7 +44,7 @@ class RecordVideo():
 
 def start_video_recording(video_filename, vial_size_input, capture_duration):
     global video_thread
-    if vial_size_input == int(8):
+    if vial_size_input == int(8): # length of video changes for vial size
 
         video_thread = RecordVideo(video_filename, capture_duration=5)
         video_thread.start()
